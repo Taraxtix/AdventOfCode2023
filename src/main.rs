338f74f9b7bbf9c4ae6373973546c960b1d4f9ea
Taraxtix <antonin.brauer@yahoo.fr>
2023-12-03 22:04:00 +0100

    Day01 - Part1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+const FILENAME: &str = "src/input.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let file = std::fs::read_to_string(FILENAME).unwrap();
+    let lines = file.lines();
+    let part1_digits = lines.map(|line| {
+        let mut res = 0;
+        for c in line.chars() {
+            if c.is_numeric() { res = c.to_digit(10).unwrap() * 10; break; }
+        }
+        for c in line.chars().rev() {
+            if c.is_numeric() { res += c.to_digit(10).unwrap(); break; }
+        }
+        res
+    });
+    println!("Part1: {}", part1_digits.sum::<u32>());
 }
