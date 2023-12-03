2d3e660f47fe240a9f96d82cbafeb0bdf890ef92
Taraxtix <antonin.brauer@yahoo.fr>
2023-12-03 22:36:14 +0100

    Day01 - Part2 (Example Updated)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 const FILENAME: &str = "src/input.txt";
 
+fn get_digit(line: &str) -> u32 {
+    let mut res = 0;
+    for c in line.chars() {
+        if c.is_numeric() { res = c.to_digit(10).unwrap() * 10; break; }
+    }
+    for c in line.chars().rev() {
+        if c.is_numeric() { res += c.to_digit(10).unwrap(); break; }
+    }
+    res
+}
+
 fn main() {
     let file = std::fs::read_to_string(FILENAME).unwrap();
-    let lines = file.lines();
-    let part1_digits = lines.map(|line| {
-        let mut res = 0;
-        for c in line.chars() {
-            if c.is_numeric() { res = c.to_digit(10).unwrap() * 10; break; }
-        }
-        for c in line.chars().rev() {
-            if c.is_numeric() { res += c.to_digit(10).unwrap(); break; }
-        }
-        res
-    });
+    let part1_lines = file.lines();
+    let part1_digits = part1_lines.map(|line| get_digit(line));
+
+    let part2_file = file.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "4")
+        .replace("five", "5e")
+        .replace("six", "6")
+        .replace("seven", "7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e");
+    let part2_lines = part2_file.lines();
+    let part2_digits = part2_lines.map(|line| get_digit(line));
+
     println!("Part1: {}", part1_digits.sum::<u32>());
+    println!("Part2: {}", part2_digits.sum::<u32>());
 }
